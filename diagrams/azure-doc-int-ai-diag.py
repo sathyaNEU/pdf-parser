e954386e975ffeee81a4cbdbe10831098782a5d4
sathayNEU <balachandar.sa@northeastern.edu>
2025-01-18 23:54:39 -0500

    feat: daac completed

--- diagrams/azure-doc-int-ai-diag.py
+++ diagrams/azure-doc-int-ai-diag.py
@@ -3,7 +3,10 @@ from diagrams.aws.storage import S3
 from diagrams.azure.ml import CognitiveServices as cs
 from diagrams.programming.language import Python as py
 
-with Diagram("PDF Parsing Using Azure Document Intelligence", show=False):
+# Specify the path to save the diagram
+output_path = "artifacts/pdf_parsing_diagram"
+
+with Diagram("PDF Parsing Using Azure Document Intelligence", show=False, filename=output_path):
     python_node = py("Python")
     cs_node = cs("Document Intelligence")
     s3_node = S3("S3")
